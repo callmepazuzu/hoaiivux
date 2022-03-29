c7a0d3e9c3a89c3c031d68a755c021ed78f1471d
callmepazuzu <102613427+callmepazuzu@users.noreply.github.com>
2022-03-30 01:27:15 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "ÉT ô ét Tuyet Ank",
+  text2: "Tham gia các bước nho nhỏ để gửi tín hiệu ét ô ét giải cưu Tuyet Anh cùng Pazuzu nha.",
+  text3: "Chào em, anh đứng đây từ chiều:))",
+  text4: "Ngày hôm nay của em thế nào? em ăn cơm chưa? ăn anh không?",
+  text5: "HONG BÉ ƠI",
+  text6: "DẠ,ĂNNNN",
+  text7: "Hôm nay đi nhà nghỉ với anh nhé ! =]]",
+  text8: "Gửi cho anh",
+  text9: "DẠ, EM ĐỒNG Ý. EM YÊU ANH VL :)))",
+  text10: "OK CHỐT ĐƠN NHA =]] ",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay anh qua đón em nè =]] Còn giờ thì chờ gì mà ko inbox cho anh đi nàooo?",
+  text12: "DẠ",
 };
 
 $(document).ready(function () {
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/messages/t/2568333909937732";
           },
         });
       }
